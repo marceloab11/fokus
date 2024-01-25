6e3827b7127c0569430386a49164be05d0eb5c6c
marceloab11 <marceloab31@gmail.com>
2024-01-25 19:48:57 -0300

    inicio do temporalizador

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause')
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+
+let tempoDecorridoEmSegundos = 5;
+let intervalId = null;
+
+musica.loop = true;
+
+musicaFocoInput.addEventListener('chage', () => {
+    if(musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
 
 focoBt.addEventListener('click', () => {
    alteraContexto('foco');
@@ -44,4 +60,29 @@ function alteraContexto(contexto) {
             default:
                 break;
     }
+}
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0) {
+        zerar()
+        alert('Tempo finalizado!')
+        return
+    }
+    tempoDecorridoEmSegundos -= 1;
+    console.log(tempoDecorridoEmSegundos)
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar() {
+    if(intervalId){
+    zerar();
+    return
+}
+    intervalId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar() {
+    clearInterval(intervalId);
+    intervalId = null
 }
