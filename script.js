cfb3089f14f20153854310313c15a88fef58defa
marceloab11 <marceloab31@gmail.com>
2024-01-26 11:00:13 -0300

    Audio adicionado

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
 const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica');
-const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const musica = new Audio('./sons/luna-rise-part-one.mp3');
+const musicaPlay = new Audio('./sons/play.wav');
+const musicaPause = new Audio('./sons/pause.mp3');
+const musicaFinish = new Audio('./sons/beep.mp3');
 
 let tempoDecorridoEmSegundos = 5;
 let intervalId = null;
 
 musica.loop = true;
 
-musicaFocoInput.addEventListener('chage', () => {
+musicaFocoInput.addEventListener('change', () => {
     if(musica.paused) {
         musica.play()
     } else {
@@ -64,6 +67,7 @@ function alteraContexto(contexto) {
 
 const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0) {
+        musicaFinish.play();
         zerar()
         alert('Tempo finalizado!')
         return
@@ -76,9 +80,11 @@ startPauseBt.addEventListener('click', iniciarOuPausar);
 
 function iniciarOuPausar() {
     if(intervalId){
+    musicaPause.play()
     zerar();
     return
 }
+musicaPlay.play()
     intervalId = setInterval(contagemRegressiva, 1000)
 }
 
@@ -86,3 +92,4 @@ function zerar() {
     clearInterval(intervalId);
     intervalId = null
 }
+
